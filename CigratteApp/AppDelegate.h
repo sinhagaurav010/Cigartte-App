7879295d45dbb88e46a71e7adffc84a903fafbe1
Rohit Dhawan <rohitdhawan@rohits-mac-mini.home>
2011-10-24 15:29:11 +0530

    Initial Commit

---
+++ CigratteApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CigratteApp
+//
+//  Created by Rohit Dhawan on 24/10/11.
+//  Copyright (c) 2011 rohit@bosswebtech.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
